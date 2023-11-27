114576acb57deb0094135669c6b6380c46b6b2c3
Daniela Rodríguez <32688524+RRDaniela@users.noreply.github.com>
2023-11-27 11:00:23 -0600

    ADDED: :sparkles: Added border collision.

--- p2_snake.c
+++ p2_snake.c
@@ -1,24 +1,22 @@
 #include "ripes_system.h"
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "time.h"
+#include "math.h"
 #define SW0 (0x01)
 #define SW1 (0x02)
 #define SW2 (0x04)
 #define SW3 (0x08)
-
-struct node{
-    int coords[2];
-    struct node *next;
-};	
+#define true 1
+#define false 0
 
 
-void setCoords(struct node *n, int x, int y) {
-    n->coords[0] = x;
-    n->coords[1] = y;
-};
+typedef int bool; //Definir bool
 
-volatile unsigned int * led_base = LED_MATRIX_0_BASE;
+volatile unsigned int * snake = LED_MATRIX_0_BASE;
+volatile unsigned int * border = LED_MATRIX_0_BASE;
+volatile unsigned int * border_2 = LED_MATRIX_0_BASE;
+volatile unsigned int * border_3 = LED_MATRIX_0_BASE;
+volatile unsigned int * border_4 = LED_MATRIX_0_BASE;
+volatile unsigned int * food = LED_MATRIX_0_BASE;
 volatile unsigned int * d_pad_up = D_PAD_0_UP;
 volatile unsigned int * d_pad_do = D_PAD_0_DOWN;
 volatile unsigned int * d_pad_le = D_PAD_0_LEFT;
@@ -26,60 +24,56 @@ volatile unsigned int * d_pad_ri = D_PAD_0_RIGHT;
 
 void main()
 {
-    
-    //Initialize nodes
-    struct node *head;
-    struct node *body_1 = NULL;
-    struct node *body_2 = NULL;
-    struct node *body_3 = NULL;
-    struct node *body_4 = NULL;	
-    
-    //Allocate memory
-    body_1 = malloc(sizeof(struct node));
-    body_2 = malloc(sizeof(struct node));
-    body_3 = malloc(sizeof(struct node));
-    body_4 = malloc(sizeof(struct node));
-    
-    //Assign body coordinates
-    setCoords(body_1, 0, 3);
-    setCoords(body_2, 0, 2);
-    setCoords(body_3, 0, 1);
-    setCoords(body_4, 0, 0);
-	
-    
-    //Connecting nodes
-    body_1->next = body_2;
-    body_2->next = body_3;
-    body_3->next = body_4;
-    body_4->next = NULL;
-    
-    head = body_1;
-
-    
 unsigned int mask = 0;
 int a;
-int direction = 0;
+int nFoodX = 15;
+int nFoodY = 10;
+int index = nFoodY * LED_MATRIX_0_WIDTH + nFoodX;
+food += index;
+*food = 0x8fce00;
+border_2 += 840;
+snake+=30;
+border_4 += 34;
+bool isDead = false;
+int snakeBody[875];
+//Pintar los bordes de arriba y abajo
+for(int i = 0; i<35; i++){ 
+*border = 0xc800ce; 
+*border_2 = 0xc800ce; 
+border = border + 1;
+border_2 = border_2+1;
+}
+//Pintar los bordes de de los lados
+for(int i=0; i<25; i++){
+*border_3 = 0xc800ce; 
+*border_4 = 0xc800ce;
+border_3 = border_3+35;
+border_4 = border_4+35;
+}
 while(1)
 {
-    for(int i = 0; i< 10000; i++){
-        a = i;
-    }
-if(*d_pad_up == 1 || direction == 1){
-    direction = 1;
-    led_base -= LED_MATRIX_0_WIDTH;
+for(int i = 0; i<10000; i++){
+a = i;
 }
-if(*d_pad_do == 1 || direction == 2){
-    direction = 2;
-    led_base += LED_MATRIX_0_WIDTH;
+// Verificar si la serpiente puede moverse hacia arriba y no toca el color indicado
+if (*d_pad_up == 1 && *(snake - LED_MATRIX_0_WIDTH) != 0xc800ce) {
+    snake -= LED_MATRIX_0_WIDTH;
 }
-if(*d_pad_le == 1 || direction == 3){
-    direction = 3;
-    led_base -= 1;
-    }
-if(*d_pad_ri == 1 || direction == 4){
-    direction = 4;
-    led_base += 1;
+
+// Verificar si la serpiente puede moverse hacia abajo y no toca el color indicado
+if (*d_pad_do == 1 && *(snake + LED_MATRIX_0_WIDTH) != 0xc800ce) {
+    snake += LED_MATRIX_0_WIDTH;
 }
-*led_base = 0xC70D0F;
+
+// Verificar si la serpiente puede moverse hacia la izquierda y no toca el color indicado
+if (*d_pad_le == 1 && *(snake - 1) != 0xc800ce) {
+    snake -= 1;
+}
+
+// Verificar si la serpiente puede moverse hacia la derecha y no toca el color indicado
+if (*d_pad_ri == 1 && *(snake + 1) != 0xc800ce) {
+    snake += 1;
+}
+*snake = 0x00e8ff;
 }
 }
