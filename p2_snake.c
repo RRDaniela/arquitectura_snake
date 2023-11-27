d44fdc0549546bd03cef64bbf6e5947dff77ca33
Daniela Rodríguez <32688524+RRDaniela@users.noreply.github.com>
2023-11-27 12:29:31 -0600

    ADDED: :sparkles: Added isDead confirmation

--- p2_snake.c
+++ p2_snake.c
@@ -11,7 +11,10 @@
 
 typedef int bool; //Definir bool
 
-volatile unsigned int * snake = LED_MATRIX_0_BASE;
+//Definir cabeza y cola de la serpiente
+volatile unsigned int * snake_head = LED_MATRIX_0_BASE;
+volatile unsigned int * snake_tail = LED_MATRIX_0_BASE;
+
 volatile unsigned int * border = LED_MATRIX_0_BASE;
 volatile unsigned int * border_2 = LED_MATRIX_0_BASE;
 volatile unsigned int * border_3 = LED_MATRIX_0_BASE;
@@ -26,59 +29,77 @@ void main()
 {
 unsigned int mask = 0;
 int a;
+//Posición donde aparecerá la comida
 int nFoodX = 15;
 int nFoodY = 10;
 int index = nFoodY * LED_MATRIX_0_WIDTH + nFoodX;
 food += index;
 *food = 0x8fce00;
+
+//Borde 2
 border_2 += 840;
+
+//Longitud inicial de la serpiente
 int snake_length= 4;
+
+//Dirección inicial de la serpiente
 int direction = 0;
-snake+=437;
+
+//Posición inicial de la cabeza de la serpiente
+snake_head+=437;
+
+//Borde 4
 border_4 += 34;
+
+//Booleano para saber si ya murió la serpiente
 bool isDead = false;
-int snakeBody[875];
+
+int snake_headBody[875];
 //Pintar los bordes de arriba y abajo
 for(int i = 0; i<35; i++){ 
-*border = 0xc800ce; 
-*border_2 = 0xc800ce; 
-border = border + 1;
-border_2 = border_2+1;
+    *border = 0xc800ce; 
+    *border_2 = 0xc800ce; 
+    border = border + 1;
+    border_2 = border_2+1;
 }
+
 //Pintar los bordes de de los lados
 for(int i=0; i<25; i++){
-*border_3 = 0xc800ce; 
-*border_4 = 0xc800ce;
-border_3 = border_3+35;
-border_4 = border_4+35;
+    *border_3 = 0xc800ce; 
+    *border_4 = 0xc800ce;
+    border_3 = border_3+35;
+    border_4 = border_4+35;
 }
-while(1)
+
+while(1 && !isDead)
 {
 for(int i = 0; i<10000; i++){
 a = i;
 }
-        // Verificar y actualizar la dirección basada en la entrada del usuario
-        if (*d_pad_up == 1 && direction != 1) {
-            direction = 0; // Mover hacia arriba
-        } else if (*d_pad_do == 1 && direction != 0) {
-            direction = 1; // Mover hacia abajo
-        } else if (*d_pad_le == 1 && direction != 3) {
-            direction = 2; // Mover hacia la izquierda
-        } else if (*d_pad_ri == 1 && direction != 2) {
-            direction = 3; // Mover hacia la derecha
-        }
+// Verificar y actualizar la dirección basada en la entrada del usuario
+if (*d_pad_up == 1 && direction != 1) {
+direction = 0; // Mover hacia arriba
+} else if (*d_pad_do == 1 && direction != 0) {
+direction = 1; // Mover hacia abajo
+} else if (*d_pad_le == 1 && direction != 3) {
+direction = 2; // Mover hacia la izquierda
+} else if (*d_pad_ri == 1 && direction != 2) {
+direction = 3; // Mover hacia la derecha
+}
 
 // Actualizar la posición de la cabeza de la serpiente según la dirección
-if (direction == 0 && *(snake - LED_MATRIX_0_WIDTH) != 0xc800ce && *(snake - LED_MATRIX_0_WIDTH) != 0x00e8ff) {
-    snake -= LED_MATRIX_0_WIDTH;
-} else if (direction == 1 && *(snake + LED_MATRIX_0_WIDTH) != 0xc800ce && *(snake + LED_MATRIX_0_WIDTH) != 0x00e8ff) {
-    snake += LED_MATRIX_0_WIDTH;
-} else if (direction == 2 && *(snake - 1) != 0xc800ce && *(snake - 1) != 0x00e8ff) {
-    snake -= 1;
-} else if (direction == 3 && *(snake + 1) != 0xc800ce && *(snake + 1) != 0x00e8ff) {
-    snake += 1;
+if (direction == 0 && *(snake_head - LED_MATRIX_0_WIDTH) != 0xc800ce && *(snake_head - LED_MATRIX_0_WIDTH) != 0x00e8ff) {
+snake_head -= LED_MATRIX_0_WIDTH;
+} else if (direction == 1 && *(snake_head + LED_MATRIX_0_WIDTH) != 0xc800ce && *(snake_head + LED_MATRIX_0_WIDTH) != 0x00e8ff) {
+snake_head += LED_MATRIX_0_WIDTH;
+} else if (direction == 2 && *(snake_head - 1) != 0xc800ce && *(snake_head - 1) != 0x00e8ff) {
+snake_head -= 1;
+} else if (direction == 3 && *(snake_head + 1) != 0xc800ce && *(snake_head + 1) != 0x00e8ff) {
+snake_head += 1;
+}else{
+    isDead = true; }
+
+*snake_head = 0x00e8ff;
+}
 }
 
-*snake = 0x00e8ff;
-}
-}
